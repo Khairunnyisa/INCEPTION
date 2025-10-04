b62b25344b876fc4895bcfe400e85db6bedd0ba3
Khairunnisa Batubara <khairunnisabatubara@Khairunnisa.local>
2025-10-04 22:04:12 +0700

    add slider section

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ document.querySelectorAll(".testi-scroll").forEach(scroll => {
     scroll.appendChild(clone);
   }
 });
+
+
+// SLIDER FOOD
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const carousel = document.querySelector('.food-carousel');
+  const nextBtn = document.querySelector('.custom-next');
+  const prevBtn = document.querySelector('.custom-prev');
+
+  const cardWidth = document.querySelector('.card-food').offsetWidth + 16; // width + gap
+  const visibleCards = 3; // jumlah card penuh yang mau kelihatan
+  let currentIndex = 0;
+
+  nextBtn.addEventListener('click', () => {
+    if (currentIndex < carousel.children.length - visibleCards) {
+      currentIndex++;
+      carousel.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+    }
+  });
+
+  prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      carousel.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+    }
+  });
+});
+
